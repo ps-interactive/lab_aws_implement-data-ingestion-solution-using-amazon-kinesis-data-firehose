90450acf3a3568c7ba280122668a1bf135be2069
Derek Weatherford <derekweatherford@gmail.com>
2021-06-23 10:38:17 -0600

    Update sensorstream.py
    Updating to boto3 in response to https://app.asana.com/0/1177532720315596/1200502991224581

--- sensorstream.py
+++ sensorstream.py
@@ -2,9 +2,18 @@ import json
 import datetime
 import random
 import testdata
-from boto import kinesis
+import boto3
+from botocore.config import Config
 
-kinesis = kinesis.connect_to_region("us-west-2")
+my_config = Config(
+    region_name = 'us-west-2',
+    retries = {
+        'max_attempts': 10,
+        'mode': 'standard'
+    }
+)
+
+kinesis = boto3.client('kinesis', config=my_config)
 
 def getData(iotName, lowVal, highVal):
    data = {}
@@ -16,9 +25,9 @@ while 1:
    rnd = random.random()
    if (rnd < 0.01):
       data = json.dumps(getData("DemoSensor", 100, 120))  
-      kinesis.put_record("RawStreamData", data, "DemoSensor")
-      print '***************************** anomaly ************************* ' + data
+      kinesis.put_record(StreamName="RawStreamData", Data=data, PartitionKey="DemoSensor")
+      print('***************************** anomaly ************************* ' + data)
    else:
       data = json.dumps(getData("DemoSensor", 10, 20))  
-      kinesis.put_record("RawStreamData", data, "DemoSensor")
-      print data
+      kinesis.put_record(StreamName="RawStreamData", Data=data, PartitionKey="DemoSensor")
+      print(data)
